6a11ee329edfc9b4b0f9ac7c0f0cd37d811588ed
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-01-27 15:41:39 +0100

    add new components

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { AboutmeComponent } from "../aboutme/aboutme.component";
+import { WorksComponent } from "../works/works.component";
 
 @Component({
   selector: 'app-main',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, AboutmeComponent, WorksComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
